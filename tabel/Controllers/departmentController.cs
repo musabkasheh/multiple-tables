f2594f8155d021b8ea2853a67db0f2f1ede9cb87
musabkasheh <user@MUSAB>
2023-06-04 04:03:19 +0300

    Add project files.

---
+++ tabel/Controllers/departmentController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using tabel.Models;
+namespace tabel.Controllers
+{
+    public class departmentController : Controller
+    {
+        // GET: department
+        public ActionResult Index()
+        {
+
+            tbl_employeeEntities1 employeecon = new tbl_employeeEntities1();
+            List<department> departments = employeecon.departments.ToList();
+            return View(departments);
+            
+        }
+    }
+}
---
+++ tabel/Controllers/employeeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using tabel.Models;
+
+
+namespace tabel.Controllers
+{
+    public class employeeController : Controller
+    {
+        // GET: employee
+        public ActionResult Index()
+        {
+            tbl_employeeEntities tbl = new tbl_employeeEntities();
+            List<employee> tab = tbl.employees.ToList();
+            return View(tab);
+        }
+        public ActionResult details(int id)
+        {
+            tbl_employeeEntities empl = new tbl_employeeEntities();
+            employee employee = empl.employees.Single(emp => emp.id == id);
+
+            return View(employee);
+        }
+    }
+}
